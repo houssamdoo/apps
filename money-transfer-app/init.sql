352fb421f99723f309f47ef9241e0489651e7cdc
houssam.dib <dhoussam.dib@gmail.com>
2025-07-15 14:41:13 +0000

    Add pgadmin

--- money-transfer-app/init.sql
+++ money-transfer-app/init.sql
@@ -4,5 +4,8 @@ CREATE TABLE "Accounts" (
     "Balance" NUMERIC(18, 2) NOT NULL
 );
 
-INSERT INTO "Accounts" ("Owner", "Balance") VALUES ('Alice', 1000.00);
-INSERT INTO "Accounts" ("Owner", "Balance") VALUES ('Bob', 500.00);
+INSERT INTO "Accounts" ("Owner", "Balance") VALUES ('Amar', 1000.00);
+INSERT INTO "Accounts" ("Owner", "Balance") VALUES ('Zoubir', 500.00);
+INSERT INTO "Accounts" ("Owner", "Balance") VALUES ('Mounir', 7000.00);
+INSERT INTO "Accounts" ("Owner", "Balance") VALUES ('Bouraya', 700000.00);
+INSERT INTO "Accounts" ("Owner", "Balance") VALUES ('Serine', 5000.00);
